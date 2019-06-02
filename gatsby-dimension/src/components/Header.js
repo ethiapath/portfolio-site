f982f34b6e453ee7d8ac82225c87b39902d7c416
Hargobind Atwal <ethaipath@gmail.com>
2019-06-01 21:01:38 -0700

    added a project section

--- gatsby-dimension/src/components/Header.js
+++ gatsby-dimension/src/components/Header.js
@@ -8,15 +8,16 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Hargobind Atwal</h1>
+                <p>Full Stack Web Developer fresh out of <a href="www.lambdaschool.com">Lambda School’s</a> fast paced 30+ week curriculum. Audio engineering and IT background, with a knack for producing effective solutions to technical bugs or issues. Can build production ready full stack applications with tight deadlines. 
+</p>
             </div>
         </div>
         <nav>
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('projects')}}>Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
