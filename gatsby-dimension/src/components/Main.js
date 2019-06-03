1f62b7c0c3f7a07290152c8a46c5410cce6e30e1
Hargobind Atwal <ethaipath@gmail.com>
2019-06-03 11:06:01 -0700

    new starter

--- gatsby-dimension/src/components/Main.js
+++ gatsby-dimension/src/components/Main.js
@@ -16,8 +16,7 @@ class Main extends React.Component {
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Intro</h2>
           <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <p>Full Stack Web Developer fresh out of <a href="www.lambdaschool.com">Lambda School’s</a> fast paced 30+ week curriculum. Audio engineering and IT background, with a knack for producing effective solutions to technical bugs or issues. Can build production ready full stack applications with tight deadlines. By the way, check out my <a href="#work">awesome work</a>.</p>
           {close}
         </article>
 
